bd86c948fbaedbf2c139a46a7da591363aba7c68
heyded <96891488+heyded@users.noreply.github.com>
2023-08-14 12:09:23 +0300

    Comit

---
+++ Shop/Shop.Presentation.Web/obj/Release/netcoreapp3.1/PubTmp/EFSQLScripts/Shop.DataAccess.EFCore.ShopDbContext.sql
@@ -0,0 +1,68 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230429160626_ShopDbCreation')
+BEGIN
+    CREATE TABLE [Categories] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [ImagePath] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Categories] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230429160626_ShopDbCreation')
+BEGIN
+    CREATE TABLE [Links] (
+        [CategoryId] int NOT NULL,
+        [ProductId] int NOT NULL,
+        CONSTRAINT [PK_Links] PRIMARY KEY ([CategoryId], [ProductId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230429160626_ShopDbCreation')
+BEGIN
+    CREATE TABLE [Orders] (
+        [OrderId] int NOT NULL IDENTITY,
+        [ProductId] int NOT NULL,
+        [UserEmail] nvarchar(max) NULL,
+        CONSTRAINT [PK_Orders] PRIMARY KEY ([OrderId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230429160626_ShopDbCreation')
+BEGIN
+    CREATE TABLE [Products] (
+        [Id] int NOT NULL,
+        [Name] nvarchar(max) NULL,
+        [Description] nvarchar(max) NULL,
+        [Price] decimal(18,2) NOT NULL,
+        [ImagePath] nvarchar(max) NULL,
+        CONSTRAINT [PK_Products] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20230429160626_ShopDbCreation')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20230429160626_ShopDbCreation', N'3.1.32');
+END;
+
+GO
+
--- Shop/Shop.Presentation.Web/obj/Release/netcoreapp3.1/Shop.Presentation.Web.AssemblyInfo.cs
+++ Shop/Shop.Presentation.Web/obj/Release/netcoreapp3.1/Shop.Presentation.Web.AssemblyInfo.cs
@@ -12,6 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("426f2e6e-d2f0-4561-81e5-a37f8f153697")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop.Presentation.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
